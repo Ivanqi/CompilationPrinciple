1a945677f440ca61aef04f04064a415816605ae8
Ivanqi <512350225@qq.com>
2020-12-02 22:08:14 +0800

    【开发】增加新的测试案例

--- RegexEngine/tests/LLParser_test/LLParser_test.cpp
+++ RegexEngine/tests/LLParser_test/LLParser_test.cpp
@@ -6,6 +6,16 @@ void test_case_1() {
     LLParser::parse(script1, SampleGrammar::statementGrammar());
 }
 
+void test_case_2() {
+    string script2 = "int a = 0; a>3;";
+    LLParser::parse(script2, SampleGrammar::statementGrammar());
+}
+
+void test_case_3() {
+    string script3 = "int a = 0; if(a<3) a=3;";
+    LLParser::parse(script3, SampleGrammar::commonLexerGrammar());
+}
+
 int main() {
 
     test_case_1();
