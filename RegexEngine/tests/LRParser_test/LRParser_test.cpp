b591943d85dadb91a3f6c75ff80c76330783dfb6
Ivanqi <512350225@qq.com>
2021-01-04 10:43:28 +0800

    【开发】优化代码

--- RegexEngine/tests/LRParser_test/LRParser_test.cpp
+++ RegexEngine/tests/LRParser_test/LRParser_test.cpp
@@ -18,6 +18,41 @@ void test_case_1() {
     LRParser::parse(script1, SampleGrammar::simpleLeftRecursiveExpressionGrammar());
 }
 
+void test_case_2() {
+    /**
+     * 示例2
+     *  带有加减乘除的语法，并且支持标识符
+     *  NFA状态数：32，DFA状态数：17
+     * 
+     *  语法：
+     *  expression	: add ;
+     *  add		: mul | add ('+' | '-') mul ;
+     *  mul		: pri | mul ('*' | '/') pri ;
+     *  pri		: ID | INT_LITERAL | LPAREN expression RPAREN ;
+     */
+    string script2 = "a - 3 / (4 + 5)";
+    LRParser::parse(script2, SampleGrammar::leftRecursiveExpressionGrammar());
+}
+
+void test_case_3() {
+    /**
+     * 示例3：
+     *  更加完整的表达式语法
+     *  NFA状态数：67，DFA状态数：33
+     * 
+     * 语法：
+     *  expression	: assign ;
+     *  assign	: equal | assign '=' equal ;
+     *  equal	: rel | equal ('==' | '!=') rel ;
+     *  rel		: add | rel ('>=' | '>' | '<=' | '<') add ;
+     *  add		: mul | add ('+' | '-') mul ;
+     *  mul		: pri | mul ('*' | '/') pri ;
+     *  pri		: ID | INT_LITERAL | LPAREN expression RPAREN ;
+     */
+    string script3 = "a = 2 - 3 > (4 + 5)"; //带有赋值，比较等运算
+    LRParser::parse(script3, SampleGrammar::fullLeftRecursiveExpressionGrammar());
+}
+
 int main() {
 
     test_case_1();
