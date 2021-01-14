09f33aa8e85f065cae7366d3e0b5cde56e2cfd7e
Ivanqi <512350225@qq.com>
2021-01-14 22:23:10 +0800

    【开发】增加新项目

---
+++ LLVMJIT/llvmjit.h
@@ -0,0 +1,38 @@
+#ifndef LLVMJIT_H
+#define LLVMJIT_H
+
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/iterator_range.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/LambdaResolver.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Mangler.h"
+#include "llvm/Support/DynamicLibrary.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Target/TargetMachine.h"
+
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace llvm 
+{
+namespace
+{
+    class LLVMJIT
+    {
+        public:
+            
+    };
+};  // orc
+};  // llvm
+
+#endif
