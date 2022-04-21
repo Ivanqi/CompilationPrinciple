e9002c048e050e5d1fe67844ea9bff745c26da9e
Ivanqi <512350225@qq.com>
2022-04-21 17:31:26 +0800

    【开发】完善 brainfuck-jit-interpreter

---
+++ brainfuck-jit-interpreter/benchmark.py
@@ -0,0 +1,57 @@
+import logging
+import shlex
+import subprocess
+import sys
+import threading
+import os
+import time
+
+default_benchmark_time = 10
+constant_idx_zer = 0
+constant_idx_one = 1
+
+def exec_cmd(command, file_name, timeout):
+    try:
+        with open(file_name, "w+") as f:
+            cmd = subprocess.Popen(shlex.split(command), shell=False, stdout=f, stderr=subprocess.PIPE, universal_newlines=True)
+            _thread_command(cmd, timeout)
+    except subprocess.SubprocessError as error:
+        print("Calledprocerr: %s", err)
+
+def _thread_command(task, timeout):
+    task_thread = threading.Thread(target=task.wait)
+    task_thread.start()
+    task_thread.join(timeout)
+
+    if task_thread.is_alive():
+        task.kill()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        # 运行默认基准测试
+        print('\nBenchmark for %s seconds: (higher score is better)' % default_benchmark_time)
+        exec_cmd('./interpreter ./bfs/FIB.bf', '.out_interpreter', default_benchmark_time)
+        exec_cmd('./interpreter ./bfs/FIB.bf --jit', '.out_jit', default_benchmark_time)
+
+        # 比较
+        completedprocess = subprocess.run('wc -l .out_interpreter .out_jit', shell=True, capture_output=True)
+        print(str(completedprocess.stdout, 'utf-8'))
+        os.remove('.out_interpreter')
+        os.remove('.out_jit')
+        sys.exit(0)
+    else:
+        # 运行可选基准测试
+        if sys.argv[constant_idx_one] == "mandelbrot":
+            time_dict = []
+            def _exec_case(command):
+                print('\nCommand ' + command + ': \n')
+                start_time = time.time()
+                subprocess.call(shlex.split(command))
+                time_dict.append(time.time() - start_time)
+            
+            _exec_case('./interpreter ./bfs/MANDELBROT.bf')
+            _exec_case('./interpreter ./bfs/MANDELBROT.bf --jit')
+
+            print('\nBenchmark Result: (lower time is better)\n' + 
+                "{:>10.3f}".format(time_dict[constant_idx_zer]) + 's interpreter\n' +
+                "{:>10.3f}".format(time_dict[constant_idx_one]) + 's jit\n')
--- brainfuck-jit-interpreter/interpreter.cpp
+++ brainfuck-jit-interpreter/interpreter.cpp
@@ -108,3 +108,345 @@ struct BFState {
     }
 };
 
+void bfJITCompile(std::vector<char> *program, BFState *state) {
+    // 辅助匿名函数
+    auto _appendBytecode = [](auto& byteCode, auto& machineCode) {
+        machineCode.insert(machineCode.end(), byteCode.begin(), byteCode.end());
+    }
+
+    auto _resolvePtrAddr = [](auto ptrAddr) -> auto {
+        // 小端序
+        return std::vector<uint8_t> {
+            static_cast<uint8_t>(ptrAddr & 0xff),
+            static_cast<uint8_t>((ptrAddr & 0xff00) >> 8),
+            static_cast<uint8_t>((ptrAddr & 0xff0000) >> 16),
+            static_cast<uint8_t>((ptrAddr & 0xff000000) >> 24), 
+            static_cast<uint8_t>((ptrAddr & 0xff00000000) >> 32),
+            static_cast<uint8_t>((ptrAddr & 0xff0000000000) >> 40),
+            static_cast<uint8_t>((ptrAddr & 0xff000000000000) >> 48),
+            static_cast<uint8_t>((ptrAddr & 0xff00000000000000) >> 56), 
+        };
+    };
+
+    auto _resolveAddrDiff = [](auto addrDiff) -> auto {
+        // 小端序
+        return std::vector<uint8_t> {
+            static_cast<uint8_t>(addrDiff & 0xff),
+            static_cast<uint8_t>((addrDiff & 0xff00) >> 8),
+            static_cast<uint8_t>((addrDiff & 0xff0000) >> 16),
+            static_cast<uint8_t>((addrDiff & 0xff000000) >> 24),
+        };
+    };
+
+    auto _relocateAddrOfPrintFunc = [&] (auto &byteCode, auto &machineCode, size_t offsetBytesFromLast) {
+        auto printCallOffset = _resolveAddrDiff(
+            static_cast<uint32_t>(-(machineCode.size() + byteCode.size() - offsetBytesFromLast + 4))
+        );
+    };
+
+    // 静态例程定义
+    const std::vector<uint8_t> staticFuncBody {
+        // 标准输出功能（当前偏移=0）
+        /**
+            movl $0x2000004, %eax
+            movl $0x1, %edi
+            movq %r10, %rsi
+            movq %r11, %rdx
+            syscall
+            retq
+        */
+#if __APPLE__
+    0xb8, 0x4, 0x0, 0x0, 0x2,
+#elif __linux__
+    0xb8, 0x1, 0x0, 0x0, 0x0,
+#endif
+    0xbf, 0x1, 0x0, 0x0, 0x0,
+    0x4c, 0x89, 0xd6,
+    0x4c, 0x89, 0xda,
+    0xf, 0x5,
+    0xc3,      
+    };
+
+    // 序言
+    std::vector<uint8_t> machineCode {
+        // 将动态指针保存在%rbx中
+        0x48, 0xbb, // 内存插槽
+    };
+
+    std::vector<size_t> jmpLocIndex {};
+
+    // 解析基指针、重新定位和前置静态函数体
+    auto basePtrBytes = _resolvePtrAddr(reinterpret_cast<size_t>(state->ptr));
+    machineCode.insert(machineCode.end(), basePtrBytes.begin(), basePtrBytes.end());
+    machineCode.insert(machineCode.begin(), staticFuncBody.begin(), staticFuncBody.end());
+
+    // 编码
+    for (auto tok = program->cbegin(); tok != program->cend(); ++tok) {
+        size_t n = 0;
+        auto ptrAddr = reinterpret_cast<size_t>(state->ptr);
+
+        switch (*tok) {
+            case '+': {
+                for (n = 0; *tok == '+'; ++n, ++tok);
+                std::vector<uint8_t> byteCodee {
+                    0x80, 0x3, static_cast<uint8_t>(n),         // addb $0x1, (%rbx)
+                };
+                _appendBytecode(byteCodee, machineCode);
+                --tok;
+                break;
+            }
+            case '-': {
+                for (n = 0; *tok == '-'; ++n, ++tok);
+                std::vector<uint8_t> byteCode {
+                    0x80, 0x2b, static_cast<uint8_t>(n),        // subb $0x1, (%rbx)
+                };
+                _appendBytecode(byteCode, machineCode);
+                --tok;
+                break;
+            }
+            case '>': {
+                for (n = 0; *tok == '>'; ++n, ++tok);
+                std::vector<uint8_t> byteCode {
+                    0x48, 0x83, 0xc3, static_cast<uint8_t>(n),  // add $0x1, %rbx
+                };
+                _appendBytecode(byteCode, machineCode);
+                --tok;  // 抵消主循环中的tok++
+                break;
+            }
+            case '<': {
+                for (n = 0; *tok == '<'; ++n, ++tok);
+                std::vector<uint8_t> byteCode {
+                    0x48, 0x83, 0xeb, static_cast<uint8_t>(n),  // sub $0x1, %rbx
+                };
+                _appendBytecode(byteCode, machineCode);
+                --tok;  // 抵消主循环中的tok++
+                break;
+            }
+            case ',': {
+                /**
+                    movl $0x2000003, %eax
+                    movl $0x0, %edi
+                    movq %rbx, %rsi
+                    movl $0x1, %edx
+                    syscall
+                */
+                std::vector<uint8_t> byteCode {
+#if __APPLE__
+                    0xb8, 0x3, 0x0, 0x0, 0x2,
+#elif __linux__
+                    0xb8, 0x0, 0x0, 0x0, 0x0,
+#endif
+                    0xbf, 0x0, 0x0, 0x0, 0x0,
+                    0x48, 0x89, 0xde,
+                    0xba, 0x1, 0x0, 0x0, 0x0,
+                    0xf, 0x5,                    
+                };
+                _appendBytecode(byteCode, machineCode);
+                break;
+            }
+            case '.' {
+                /**
+                    movq (%rbx), %r12
+                    movq %r12, (%r10,%r11)
+                    incq %r11
+                    cmpq $1024, %r11
+                    jne 8
+                    callq <print>
+                    xorq %r11, %r11
+                */
+                std::vector<uint8_t> byteCode {
+                    // 为stdout设置一个简单的缓冲区
+                    0x4c, 0x8b, 0x23,
+                    0x4f, 0x89, 0x24, 0x1a,
+                    0x49, 0xff, 0xc3,
+                    0x49, 0x81, 0xfb, 0x0, 0x4, 0x0, 0x0,
+                    0x75, 0x8,
+                    // flush
+                    0xe8, /* mem slot */
+                    // reset counter.
+                    0x4d, 0x31, 0xdb,
+                };
+                _relocateAddrOfPrintFunc(byteCode, machineCode, 3);
+                _appendBytecode(byteCode, machineCode);
+                break;
+            }
+            case '[': {
+                /*
+                    cmpb $0x0, (%rbx)
+                    je <>
+                */
+                std::vector<uint8_t> byteCode{
+                    0x80, 0x3b, 0x0,
+                    0xf, 0x84, 0x0, 0x0, 0x0, 0x0, /* near jmp */
+                };
+                // 记录跳转位置
+                _appendBytecode(byteCode, machineCode);
+                jmpLocIndex.push_back(machineCode.size());
+                break;
+            }
+            case ']': {
+                /*
+                    cmpb $0x0, (%rbx)
+                    jne <>
+                */
+                std::vector<uint8_t> byteCode { 
+                    0x80, 0x3b, 0x0,
+                    0xf, 0x85, 0x0, 0x0, 0x0, 0x0, /* near jmp */
+                };
+                _appendBytecode(byteCode, machineCode);
+
+                // 计算实际偏移量
+                auto bDiff = _resolveAddrDiff(static_cast<uint32_t>(jmpLocIndex.back() - machineCode.size()));
+                auto fDiff = _resolveAddrDiff(static_cast<uint32_t>(machineCode.size() - jmpLocIndex.back()));
+
+                // 重新定位生成的机器代码的内存地址
+                machineCode.erase(machineCode.end() - 4, machineCode.end());
+                machineCode.insert(machineCode.end(), bDiff.begin(), bDiff.end());
+
+                // 重新定位相应的前一个“[”
+                machineCode.erase(machineCode.begin() + jmpLocIndex.back() - 4, machineCode.begin() + jmpLocIndex.back());
+                machineCode.insert(machineCode.begin() + jmpLocIndex.back() - 4, fDiff.begin(), fDiff.end());
+                jmpLocIndex.pop_back();
+
+                // 减少不必要的'cmp'，专用于“]]]等模式
+                auto ctok = tok + 1;
+                for (n = 0; *ctok == ']'; ++n, ++ctok);
+                if (n > 0) {
+                    std::vector<uint8_t> byteCode {
+                        0xeb, static_cast<uint8_t>(n * 11 - 2),
+                    };
+                    _appendBytecode(byteCode, machineCode);
+                }
+                break;
+            }
+        }
+    }
+
+    // 后记
+    // 主要是恢复以前的pc，刷新标准输出缓冲区
+    /**
+        cmpq $0, %r11
+        je 8
+        callq <print>
+        jmpq *(%rsp)
+    */
+    std::vector<uint8_t> byteCode {
+        0x49, 0x83, 0xfb, 0x0,
+        0x74, 0x8,
+        0xe8, /* mem slot */
+        0xff, 0x24, 0x24,
+    };
+    _relocateAddrOfPrintFunc(byteCode, machineCode, 3);
+    _appendBytecode(byteCode, machineCode);
+
+    // 动态执行
+    VM(&machineCode, staticFuncBody.size()).exec();
+}
+
+void bfInterpret(const char *program, BFState *state) {
+    const char *loops[MAX_NESTING];
+    auto nloops = 0;
+    auto nskip = 0;
+    size_t n = 0;
+
+    while (true) {
+        // 线程开关
+        switch (*program++) {
+            case '<': {
+                for (n = 1; *program == '<'; ++n, ++program);
+                if (!nskip) state->ptr -= n;
+                break;
+            }
+
+            case '>': {
+                for (n = 1; *program == '>'; ++n, ++program);
+                if (!nskip) state->ptr += n;
+                break;
+            }
+
+            case '+': {
+                for (n = 1; *program == '+'; ++n, ++program);
+                if (!nskip) *state->ptr += n;
+                break;
+            }
+
+            case '-': {
+                for (n = 1; *program == '-'; ++n, ++program);
+                if (!nskip) *state->ptr -= n;
+                break;
+            }
+
+            case ',': {
+                if (!nskip) *state->ptr = static_cast<unsigned char>(std::getchar());
+                break;
+            }
+
+            case '.': {
+                if (!nskip) {
+                    std::cout << *state->ptr;
+                }
+                break;
+            }
+
+            case '[': {
+                if (nloops == 0) std::terminate();
+                loops[nloops++] = program;
+
+                if (!*state->ptr) ++nskip;
+                break;
+            }
+
+            case ']': {
+                if (nloops == 0) std::terminate();
+                if (*state->ptr) program = loops[nloops - 1];
+                else --nloops;
+
+                if (nskip) --nskip;
+                break;
+            }
+
+            case ' ': {
+                for (n = 1; *program == ' '; ++n, ++program);  // clear spaces.
+                break;
+            }
+
+            case '\0': {
+                return;
+            }
+        }
+    }
+}
+
+inline void bfRunInterpret(const char *sourceCode) {
+    BFState bfs;
+    bfInterpret(sourceCode, &bfs):
+}
+
+inline void bfRunJIT(std:;vector<char> *sourceCode) {
+    BFState bfs;
+    bfJITCompile(sourceCode, &bfs);
+}
+
+int main() {
+    char token;
+    std::vector<char> v {};
+
+    if (argc > 1) {
+        std::string inputSourceFileName = std::string(*(argv + 1));
+        std::ifstream f(inputSourceFileName, std::ios::binary);
+
+        while (f.is_open() && f.good() && f >> token) {
+            v.push_back(token);
+        }
+    }
+
+    if (v.size() > 0) {
+        if (argc > 2 && std::string(*(argv + 2)) == "--jit") {
+            bfRunJIT(&v);
+        } else {
+            bfRunInterpret(v.data());
+        }
+    }
+
+    return 0;
+}
